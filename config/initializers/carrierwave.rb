a02876d8267f1287b6e738baa3be58d4568789d8
micchel <msaito2112@gmail.com>
2019-07-30 19:16:52 +0900

    carrierwaveの設定

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,24 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'upload_mercari55a'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-mercari55a'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+      aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+      region: 'ap-northeast-1'
+      path_style: true
+    }
+
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
+  
+end
