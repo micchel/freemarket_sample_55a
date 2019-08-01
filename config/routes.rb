56a2f40104e4535656bbeafbb16f856037afde76
micchel <msaito2112@gmail.com>
2019-08-01 10:45:42 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,15 @@
 Rails.application.routes.draw do
+
+  root to: "items#index"
+  
+  get "mypage", to: "mypage#top"
+  get "mypage/profile", to: "mypage#profile"
+  get "mypage/card", to: "mypage#card"
+  get "mypage/information", to: "mypage#information"
+  get "logout", to: "mypage#logout"
+
+  resources :items, only: :show
+  get "sell", to: "items#sell"
+  get "sell/edit/:id", to: "items#edit_item"
+  get "transaction/buy/:id", to: "items#buy_confirm"
 end
