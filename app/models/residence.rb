d7e71e5f623d8f7a5f1c7e8338205526d5cee667
mizuno <inc.sinka@gmail.com>
2019-08-02 19:41:59 +0900

    residence-model-create

---
+++ app/models/residence.rb
@@ -0,0 +1,10 @@
+class Residence < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to_active_hash :prefecture
+belongs_to :user, optional: true
+
+validates :post_code, presence: true
+validates :prefecture_id, presence: true
+validates :city, presence: true
+validates :address, presence: true
+end
