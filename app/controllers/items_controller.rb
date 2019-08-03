a43365fa05d49b8d03a7b5319e8dc9115407a872
micchel <msaito2112@gmail.com>
2019-08-03 17:27:55 +0900

    ルーティング・コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,33 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def sell
+  def new
+    @item = Item.new
+    @parents = Category.where(ancestry: nil) #Category.where(ancestry = ?, "nil")
+    @item.item_images.build
+  end
+
+  def select
+    respond_to do |format|
+      format.html
+      format.json do
+        if params[:parent_id]
+          @children = Category.find(params[:parent_id]).children
+        else
+          @grand_children = Category.find(params[:child_id]).children
+        end
+      end
+    end
+  end
+
+  def select_grand
+    respond_to do |format|
+      format.html
+      format.json { @grand_children_info = Category.find(params[:grand_id]) }
+    end
+  end
+
+  def create
   end
 
   def edit_item
