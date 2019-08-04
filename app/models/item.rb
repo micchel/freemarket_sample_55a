606a9fbcfea12006e000ea4bd3d7e7781a30860f
micchel <msaito2112@gmail.com>
2019-08-04 22:59:26 +0900

    enumの日本語対応化+ itemモデルvalidatesの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,18 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  enum status: { "新品、未使用": 0, "未使用に近い": 1, "目立った傷や汚れなし": 2, "やや傷や汚れあり": 3, "傷や汚れあり": 4, "全体的に状態が悪い": 5 }
-  enum delivery_cost: { "送料込み(出品者負担)": 0, "着払い(購入者負担)": 1 }
-  enum delivery_date: { "1~2日で発送": 0, "2~3日で発送": 1, "4~7日で発送":2 }
+  enum status: { new_thing: 0, near_unused: 1, no_damage: 2, little_damage: 3, damage: 4, bad_thing: 5 }
+  enum delivery_cost: { postage_included: 0, cash_on_delivery: 1 }
+  enum delivery_way: { undecided: 0, mercari_bin: 1, yu_mail: 2, letter_pack: 3, regular_mail: 4, kuroneko: 5, yu_pack: 6, click_post: 7, yu_packet: 8 }
+  enum delivery_date: { ship_in_1to2days: 0, ship_in_2to3days: 1, ship_in_4to7days:2 }
+
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :explain, presence: true, length: { maximum: 1000}
+  validates :category_id, presence: true
+  validates :status, presence: true
+  validates :delivery_cost, presence: true
+  validates :delivery_way, presence: true
+  validates :delivery_prefecture, presence: true
+  validates :delivery_date, presence: true
+  validates :price, presence: true, inclusion: { in: (300..10000)}
 end
