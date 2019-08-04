fbebdb13a999597bb860f8a36ef4b66e28b877ba
0610ot <ot1107@icloud.com>
2019-08-04 19:11:24 +0900

    fixup! 各html.hamlファイルにパンくずを追加

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,37 +7,37 @@ crumb :root do
 end
 
 crumb :mypage do
-  link "マイページ", '/mypage'
+  link "マイページ", mypage_index_path
 end
 
 crumb :profile do
-  link "プロフィール", '/mypage/profile'
+  link "プロフィール", profile_mypage_index_path
   parent :mypage
 end
 
 crumb :list do
-  link "出品した商品-出品中", '/mypage/list'
+  link "出品した商品-出品中", list_mypage_index_path
   parent :mypage
 end
 
 crumb :edit do
-  link "出品商品画面", '/mypage/:id/edit'
+  link "出品商品画面", edit_mypage_path(:id)
   parent :mypage
   parent :list
 end
 
 crumb :card do
-  link "支払い方法", '/mypage/card'
+  link "支払い方法", card_mypage_path
   parent :mypage
 end
 
 crumb :information do
-  link "本人情報の登録", '/mypage/information'
+  link "本人情報の登録", information_mypage_index_path
   parent :mypage
 end
 
 crumb :logout do
-  link "ログアウト", '/mypage/logout'
+  link "ログアウト", logout_mypage_index_path
   parent :mypage
 end
 
