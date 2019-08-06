270173479757f5a35348e0f8fe0b91da6727e04b
mizuno <inc.sinka@gmail.com>
2019-08-06 15:37:38 +0900

    sign up view haml scss fix

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,20 +1,20 @@
 class SignupController < ApplicationController
-  def index
+  def signup
   end
 
-  def new
+  def menber
     @user = User.new
   end
 
   def tel
     session[:nickname] = user_params[:nickname],
-    session[:mail] = user_params[:mail],
-    session[:password] = user_params[:password]
-    session[:password_confirmation] = user_params[:password_confirmation]
-    session[:last_name] = user_params[:last_name]
-    session[:first_name] = user_params[:first_name]
-    session[:last_name_kana] = user_params[:last_name_kana]
-    session[:first_name_kana] = user_params[:first_name_kana]
+    session[:email] = user_params[:email],
+    session[:password] = user_params[:password],
+    session[:password_confimation] = user_params[:password_confimation],
+    session[:last_name] = user_params[:last_name],
+    session[:first_name] = user_params[:first_name],
+    session[:last_name_kana] = user_params[:last_name_kana],
+    session[:first_name_kana] = user_params[:first_name_kana],
     session[:birthday] = user_params["birthday(1i)"] + "-" +  user_params["birthday(2i)"] + "-" + user_params["birthday(3i)"]
     @user = User.new
   end
@@ -27,24 +27,24 @@ class SignupController < ApplicationController
 
 
   def create
-    @user = Uer.new(
-      nickname:             session[:nickname],
-      mail:                 session[:mail],
-      password:             session[:password],
-      password_confimation: session[:password_confirmation],
-      last_name:            session[:last_name],
-      first_name:           session[:first_name],
-      last_name_kana:       session[:last_name_kana],
-      first_name_kana:      sisson[:first_name_kana],
-      birthday:             session[:birthday],
-      pohone_number:        sisson[:pohone_number]
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      last_name: session[:last_name],
+      first_name: session[:first_name],
+      last_name_kana: session[:last_name_kana],
+      first_name_kana: session[:first_name_kana],
+      birthday: session[:birthday],
+      phone_number: session[:phone_number]
     )
-    @user.build_residence(user_params[:residence])
+    @user.build_residence(user_params[:residences])
     if @user.save
       session[:id] = @user.id
-      redirect_to done_signup_path
+      redirect_to  done_signup_index_path
     else
-      render '/singup/index'
+      render '/signup/signup'
     end
   end
 
@@ -55,6 +55,6 @@ class SignupController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation ,:nickname, :name_kanji, :name_kana, :birthday, :phone_number, residences: [:zip_code, :prefecture_id, :city, :address, :detail])
+    params.require(:user).permit(:email, :password, :password_confimation ,:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :phone_number, residences: [:zip_code, :prefecture_id, :city, :address, :detail])
   end
 end
