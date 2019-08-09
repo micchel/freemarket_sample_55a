6aa076004cfd40da940e1a69be6be09c4afa549f
micchel <msaito2112@gmail.com>
2019-08-09 16:25:00 +0900

    itemモデル,applicaton.js編集

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
 //= require jquery
+//= require rails-ujs
 //= require jquery_ujs
+//= require activestorage
+//= require turbolinks
 //= require_tree .
