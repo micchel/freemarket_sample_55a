849a76963617d4aede53d87f8cda6a5ca615f5c1
micchel <msaito2112@gmail.com>
2019-08-09 16:43:26 +0900

    console.log削除

--- app/assets/javascripts/image_form.js
+++ app/assets/javascripts/image_form.js
@@ -8,11 +8,7 @@ $(function() {
 
   $(".sell-form__upload-box__dropbox").on("change", 'input[type= "file"]#upload-image', function() {
     var file = $(this).prop("files")[0];
-    // console.log("show_file")
-    // console.log(file);
     new_image_files.push(file);
-    // console.log("show_new_image_files");
-    // console.log(new_image_files);
     var reader = new FileReader();
 
     var img = $(`<div class="add_img">
@@ -24,8 +20,6 @@ $(function() {
                           <a class="btn_delete">全削除</a>
                         </div>`);
     img.append(btn_wrapper);
-    // console.log("show_img");
-    // console.log(img);
 
     reader.onload = function(e) {
       img.find("img").attr({
@@ -34,20 +28,12 @@ $(function() {
     };
     reader.readAsDataURL(file);
     images.push(img);
-    // console.log("show_reader_result");
-    // console.log(reader.result);
-    // console.log("show_images");
-    // console.log(images);
 
     if (images.length <= 4) {
       $('#preview').empty();
       $.each(images, function(index, image) {
         image.data("image", index);
-        // console.log("show_image_child");
-        // console.log(image.find());
         preview.append(image);
-        // console.log("show_preview")
-        // console.log(preview)
       })
       dropzone.css({
         "width": `calc(100% - (20% * ${images.length}))`
@@ -55,8 +41,6 @@ $(function() {
     } else if (images.length == 5) {
       $("#preview").empty();
       $.each(images, function(index, image) {
-        // console.log("show_image")
-        // console.log(image);
         image.data("image", index);
         preview.append(image);
       });
@@ -73,19 +57,11 @@ $(function() {
     $(".sell-form__upload-box__dropbox").on("click", ".btn_delete", function() {
 
       var target_image = $(this).parent().parent();
-      // console.log("show_target_image");
-      // console.log(target_image);
       var target_image_num = target_image.data("image");
-      // console.log("show_target_image_num");
-      // console.log(target_image_num);
 
       target_image.remove();
       images.splice(target_image_num, 1);
-      // console.log("images_after_delete");
-      // console.log(images);
       new_image_files.splice(target_image_num, 1);
-      // console.log("imagefiles_after_delete");
-      // console.log(new_image_files);
 
       if(images.length == 0) {
         $('input[type= "file"]#upload-image').attr({
@@ -95,8 +71,6 @@ $(function() {
       if (images.length <= 4) {
         $("#preview").empty();
         $.each(images, function(index, image) {
-          // console.log("show_image");
-          // console.log(image);
           image.data("image", index);
           preview.append(image);
         })
