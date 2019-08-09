b6c0a7192498ea268baa912f5c2fb939e752b2b1
micchel <msaito2112@gmail.com>
2019-08-09 16:13:05 +0900

    マイグレーションファイル編集

--- db/migrate/20190730030448_create_items.rb
+++ db/migrate/20190730030448_create_items.rb
@@ -1,9 +1,8 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.integer :item_image_id, null: false, foreign_key: true
       t.integer :category_id, null: false, foreign_key: true
-      t.integer :brand_id, null: false
+      t.integer :brand_id
       t.string :name, null: false
       t.text :explain, null: false 
       t.integer :price, null: false
