bf41deb14a67d036cb928dab0dc2b083a0fcba32
mizuno <inc.sinka@gmail.com>
2019-08-14 03:07:51 +0900

    payjp-js-routes-create

--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,12 @@ Rails.application.routes.draw do
       get 'done'
     end
   end
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 end
